687bdd523a0da8d70fd236a37f56b3b8442851c5
Jijesh Mohan <jijeshmohan@gmail.com>
2012-12-01 23:05:12 +0530

    first epub conversion

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , mime = require('mime')
+  , fs = require('fs')
+  , exec = require('child_process').exec;
 
 var app = express();
 
@@ -17,7 +20,8 @@ app.configure(function(){
   app.set('view engine', 'ejs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express.bodyParser());
+  app.use(express.bodyParser({uploadDir: __dirname + '/uploaded',keepExtensions: true}));
+  app.use(express.limit('5mb'));
   app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
   app.use(express.session());
@@ -32,6 +36,42 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.all('/genpub',function(req,res){
+   if(req.method=='GET'){
+      res.render('uploadform', { title: 'Express' });
+   }else{
+    file = req.files.the_file;
+    // console.log(file.name);
+    // console.log(file.type);
+    // console.log(file.path);
+
+    var output_path = __dirname +"/public/" + file.name + ".epub"
+    var command = "pandoc  -o " + output_path + " " + file.path
+
+    child = exec(command,
+      function (error, stdout, stderr) {
+        if (error !== null) {
+          console.log('exec error: ' + error);
+          res.end("ERROR: "+ error);
+        }
+
+
+        var filename = path.basename(output_path);
+        var mimetype = mime.lookup(output_path);
+        res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+        res.setHeader('Content-type', mimetype);
+        var filestream = fs.createReadStream(output_path);
+
+        filestream.on('data', function(chunk) {
+          res.write(chunk);
+        });
+        filestream.on('end', function() {
+          res.end();
+        });
+    });
+
+   }
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
